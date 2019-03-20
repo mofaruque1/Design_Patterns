39b4f58c11279904029d6d4cf79eb5b77d2b5dee
Omor Faruque <mdomor.faruque@rci.rogers.ca>
2019-03-20 17:43:36 -0400

    x commit

---
+++ src/visitor_pattern/Main.java
@@ -0,0 +1,15 @@
+package visitor_pattern;
+
+public class Main {
+	public static void main(String[] args) {
+		UpVisitor up = new UpVisitor();
+		
+		FOO foo = new FOO();
+		foo.accept(up);
+		
+		BAR bar = new BAR();
+		bar.accept(up);
+				
+	}
+
+}
